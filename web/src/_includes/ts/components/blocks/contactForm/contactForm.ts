114c1f449901f9276ac1fa5ccc3a4224cc70be7b
soulelyhayden <haydensoule@outlook.com>
2021-05-28 16:32:29 -0400

    Contact form submission

--- web/src/_includes/ts/components/blocks/contactForm/contactForm.ts
+++ web/src/_includes/ts/components/blocks/contactForm/contactForm.ts
@@ -1,6 +1,9 @@
 /*
-*  Submit form to Firebase
+*  Submit contact form
 */
+
+import * as emailjs from 'emailjs-com'
+import { init } from 'emailjs-com';
 
 let form:HTMLFormElement
 
@@ -14,14 +17,34 @@ function submitForm(e:Event) {
 
 	//   Get input Values
 	let name:string = (<HTMLInputElement>form.querySelector("#Name")).value;
-	let email:string = (<HTMLInputElement>form.querySelector("#Email")).value;
+	let fromEmail:string = (<HTMLInputElement>form.querySelector("#Email")).value;
 	let message:string = (<HTMLInputElement>form.querySelector("#Message")).value;
 
-	sendContactMessage(name, email, message);
+	sendContactMessage(name, fromEmail, message);
 	form.reset();
 }
 
-// Save infos to Firebase
-function sendContactMessage(name:string, email:string, message:string) {
+function sendContactMessage(name: string, fromEmail:string, message:string) {
+	let emailSelectors:NodeListOf<Element> = form.querySelectorAll(".email-response")!;
+	emailSelectors.forEach(selector => {
+		selector.classList.add('hidden');
+	});
 
+	init("user_JHPlqxuUS8o2vXL540O8u");
+	
+	const email = {
+		from_name: name,
+		reply_to: fromEmail,
+		message: message
+	};
+	
+	emailjs.send('service_13j22mc', 'template_ircgzrg', email)
+		.then(function (response) {
+			console.log(`Message sent with response: ${response}`);
+			form.querySelector("#emailSuccess")!.classList.remove('hidden');
+		}, function (err) {
+			console.log(`Message error: ${err}`)
+			form.querySelector("#emailFailure")!.classList.remove('hidden');
+		});
 }
+
