4027ed6f3474cd85e12d0ede70f651072e4f8e0d
soulelyhayden <haydensoule@outlook.com>
2021-06-03 15:28:54 -0400

    Staging push

--- studio/src/dashboardConfig.js
+++ studio/src/dashboardConfig.js
@@ -7,9 +7,9 @@ export default {
 				sites: [
 					{
 						title: 'Production Site',
-						apiId: 'e7a7f8a4-ec3a-4e06-82a2-191027622be7',
-						buildHookId: '60b9238865c8aa1cabc87a45',
-						name: 'goofy-sammet-aec267',
+						apiId: 'd1c59088-58cc-4182-9a85-76ab54b515d2',
+						buildHookId: '60b92cc8499530276f20a106',
+						name: 'nifty-mirzakhani-49a8dd',
 					}
 				]
 			}
